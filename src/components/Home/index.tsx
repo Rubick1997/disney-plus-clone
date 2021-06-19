907852cbd8e3b77376ebf86af955692d8f7dfcdd
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-06-18 17:10:24 -0700

    viewers

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ImageSlider from '../ImageSlider';
 import styles from "./styles.module.css";
-
+import Viewers from '../Viewers';
 function Home() {
     return (
         <main className={styles.container}>
             <ImageSlider/>
+            <Viewers/>
         </main>
     )
 }
--- src/components/Viewers/index.tsx
+++ src/components/Viewers/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import styles from "./styles.module.css";
 
 function Viewers() {
-    return (
-        <div>
-            
+  const images = [
+    { service: "disney" },
+    { service: "pixar" },
+    { service: "marvel" },
+    { service: "starwars" },
+    { service: "national" },
+  ];
+  return (
+    <div className={styles.container}>
+      {images.map((item) => (
+        <div className={styles.wrap}>
+          <img src={`/img/viewers-${item.service}.png`} alt={`viewers-${item.service}`} />
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Viewers
+export default Viewers;
